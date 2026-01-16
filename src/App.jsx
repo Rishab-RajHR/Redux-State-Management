6a6b4ca814e68eff3617f68e615bb2a15819b3e5
Rishab Raj <rr0948555@gmail.com>
2026-01-16 13:43:54 +0530

    Folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Home from './components/Home'
 
 const App = () => {
   return (
     <div>
       <h2>This is App.jsx File</h2>
+      <Home />
     </div>
   )
 }
